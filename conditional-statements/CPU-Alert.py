204d669a3223f39f27f1e55f27e7a9932b0598ae
Muskan Tandel <muskantandel824@gmail.com>
2026-07-18 21:18:26 +0530

    changes done

--- conditional-statements/CPU-Alert.py
+++ conditional-statements/CPU-Alert.py
@@ -1,11 +1,16 @@
 #This is an Script for monitoring the cpu usage
 
-usage = int(input("Enter CPU Usage in (%) : "))
-if usage < 0 or usage > 100:
+try :
+   
+ usage = int(input("Enter CPU Usage in (%) : "))
+ if usage < 0 or usage > 100:
     print("Invlid CPU Usage : Enter a valid value between 0 to 100")
-elif usage > 80:
+ elif usage > 80:
     print("🔴 High CPU Alert!")
-elif usage >= 50:
-    print("🟡 Moderate CPU Usage")
-else:
-    print("🟢 CPU Usage is Normal.")          
+ elif usage >= 50:
+    print("🟡 Moderate CPU Usage.")
+ else:
+    print("🟢 CPU Usage is Normal.")   
+     
+except ValueError:
+ print("please enter numbers only .")          
