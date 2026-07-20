45593f62d434ed6e0a59fa791ac538c18e40e1ad
Muskan Tandel <muskantandel824@gmail.com>
2026-07-20 22:19:54 +0530

    cloudclean added

---
+++ Loops/CloudClean.py
@@ -0,0 +1,10 @@
+resources={"EC2":"unused","s3":"used","load-balancer":"unused","security-grp":"unused"}
+for name, status in resources.items():
+    if status == "unused":
+        print(f"Deleting {name}")
+
+# resources = {"EC2-1": "unused", "EC2-2": "active", "EC2-3": "unused"}
+
+# for name, status in resources.items():
+#     if status == "unused":
+#         print(f"Deleting {name}...")
